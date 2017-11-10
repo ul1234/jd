83dd9ae06037b404bb381d081526efa521f3f048
ul1234 <unclelim@sina.com>
2017-11-10 18:00:24 +0800

    1

--- task.py
+++ task.py
@@ -78,36 +78,50 @@ class TaskList:
                    del self.running_task[i]
                    self.wait_task.append(t)
             time.sleep(WAIT_TIME)
-
-
-
+        
 class Task:
     def __init__(self, name, account, day, start_time, priority = 'low', now = False):
         self.name = name
         self.day = day
         self.start_time = start_time
-        self.set_next_time(start_time, day, now)
+        self.next_start_time = self.get_next_time(start_time, day, now)
         self.account = account
         self.priority = priority
         self.config_file = 'task.ini'
         self.ini_file = '%s_%s.ini' % (name, account.user)
         self.ini = ConfigParser.ConfigParser()
 
-    def set_next_time(self, start_time, today, now):
-        config = self.load_config()
-        ############here?????????
+    def get_next_time(self, start_time, day, now):
+        if now:
+            next_time = 0
+        else:
+            next_time = self.load_config(key = 'next_time')
+            if next_time is None:
+                n = time.time()
+                today_start_time = time.mktime(time.strptime('%s %s' % (datetime.now().strftime('%y%m%d'), start_time), '%y%m%d %H:%M:%S'))
+                if n > today_start_time:
+                    next_time = today_start_time + 24*3600  # tomorrow
+                else:
+                    next_time = today_start_time
+        return next_time
 
-    def load_config(self):
+    def load_config(self, key = None):
         if not os.path.isfile(self.ini_file):
             return
         self.ini.read(self.ini_file)
         if not self.name in self.ini.sections():
             print_('no task [%s] found!' % self.name)
-            return []
-        items = self.ini.items(name)
+            return None
+        items = self.ini.items(self.name)
+        if key:
+            return items[key] if key in items else None
+        else:
+            return items
 
-    def save_config(self, save_file):
-        pass
+    def save_config(self, key, value):
+        if os.path.isfile(self.ini_file): self.ini.read(self.ini_file)
+        self.ini.set(self.name, key, value)
+        self.ini.write(open(self.ini_file,'w'))
 
     def prepare(self):
         pass
@@ -124,7 +138,7 @@ class Task:
 
 class SignTask(Task):
     def __init__(self, name, account, now = False):
-        start_time = '00:05'
+        start_time = '00:05:00'
         day = 'everyday'
         priority = 'low'
         Task.__init__(self, name, account, day, start_time, priority)
